f2a60e26978bda3c1885245632d4f4b53c92d028
René Kuhn <rene@getkobe.ai>
2026-04-22 12:27:54 +0200

    sync @ 12:27:54

--- internal/testutil/postgres_test.go
+++ internal/testutil/postgres_test.go
@@ -9,19 +9,7 @@ import (
 )
 
 func TestStartPostgres(t *testing.T) {
-	ctx := context.Background()
 
-	_, connStr := StartPostgres(ctx, t)
-
-	db, err := sql.Open("pgx", connStr)
-	if err != nil {
-		t.Fatalf("open db: %s", err)
-	}
-	defer db.Close()
-
-	if err := db.PingContext(ctx); err != nil {
-		t.Fatalf("ping db: %s", err)
-	}
 }
 
 func TestPostgresSchema(t *testing.T) {
