96ebb6702246f64fc2b3c53347b366d8b52ea996
René Kuhn <rene@getkobe.ai>
2026-04-22 11:55:27 +0200

    sync @ 11:55:27

---
+++ internal/testutil/postgres_test.go
@@ -0,0 +1,25 @@
+package testutil
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func TestStartPostgres(t *testing.T) {
+	ctx := context.Background()
+
+	_, connStr := StartPostgres(ctx, t)
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		t.Fatalf("open db: %s", err)
+	}
+	defer db.Close()
+
+	if err := db.PingContext(ctx); err != nil {
+		t.Fatalf("ping db: %s", err)
+	}
+}
